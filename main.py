b299ac9d9f70382c6c01fad36f1a95fa12193c28
Alejandro Cambronero <acambronero@innerspec.com>
2024-10-11 20:43:23 +0200

    configure backend to serve frontend

--- main.py
+++ main.py
@@ -1,7 +1,22 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.staticfiles import StaticFiles
+from fastapi.responses import FileResponse
 
 app = FastAPI()
 
+# Add CORS middleware
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5000"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+# Add static files
+app.mount("/static", StaticFiles(directory="static/dist"), name="static")
+
 @app.get("/")
-def read_root():
-    return {"Hello": "World"}
+async def serve_frontend():
+    return FileResponse("static/dist/index.html")
