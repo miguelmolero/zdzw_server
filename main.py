4bcd9ef4c8daebbaf6e22dbb6102beabc6d3afbe
Alejandro Cambronero <acambronero@innerspec.com>
2024-10-24 23:24:30 +0200

    Organize folders

--- main.py
+++ main.py
@@ -16,29 +16,29 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-# Verificar si el script está empaquetado por PyInstaller
+# Verify if the script is running in a PyInstaller bundle
 if getattr(sys, 'frozen', False):
-    # Carpeta temporal donde PyInstaller descomprime los archivos
+    # Temporal folder where PyInstaller stores the files
     base_path = sys._MEIPASS
 else:
-    # En desarrollo, usa la carpeta actual
+    # In development mode, the base path is the current directory
     base_path = os.path.abspath(os.path.dirname(__file__))
 
-# Ruta a los archivos estáticos (ajustado para PyInstaller)
+# Route to the static folder
 static_folder_path = os.path.join(base_path, 'static/dist')
 
-# Imprimir la ruta para depurar
-print(f"Serving static files from: {static_folder_path}")
+# Check the static folder path
+# print(f"Serving static files from: {static_folder_path}")
 
-# Montar los archivos estáticos en FastAPI
+# Mount the static folder
 app.mount("/static", StaticFiles(directory=static_folder_path), name="static")
 
-# Ruta para servir el index.html en la raíz
+# Route to serve the frontend index.html
 @app.get("/")
 async def serve_root():
     return FileResponse(os.path.join(static_folder_path, "index.html"))
 
-# Ruta catch-all para manejar otras rutas de la aplicación frontend
+# Catch all route to serve the frontend index.html
 @app.get("/{full_path:path}")
 async def serve_frontend(full_path: str):
     return FileResponse(os.path.join(static_folder_path, "index.html"))
