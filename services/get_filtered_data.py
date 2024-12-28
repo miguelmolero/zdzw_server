5ac5d4f5a62b74a26e28c414fc23bee9c1410390
Alejandro Cambronero <acambronero@innerspec.com>
2024-12-28 16:28:00 +0100

    Fix filtered types to get the correct list type instead of numpy list type

--- services/get_filtered_data.py
+++ services/get_filtered_data.py
@@ -6,10 +6,12 @@ def get_filtered_data(json_data : RecordRawData) -> RecordRawData:
     # Filtrar los datos de entrada
     window_size = 199  
     poly_order = 1 
+    amplitude_filtered = []
+    tof_filtered = []
 
     for strip in json_data.payload_data.strip_data: 
-        amplitude_filtered = savgol_filter(strip.amplitude, window_size, poly_order)
-        tof_filtered = savgol_filter(strip.tof, window_size, poly_order)
+        amplitude_filtered = savgol_filter(strip.amplitude, window_size, poly_order).tolist()
+        tof_filtered = savgol_filter(strip.tof, window_size, poly_order).tolist()
         strip.amp_damages = amplitude_filtered
         strip.tof_damages = tof_filtered
 
