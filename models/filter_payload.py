a40352765a1b01837e9e314e75c117e794646805
Alejandro Cambronero <acambronero@innerspec.com>
2025-01-28 00:55:19 +0100

    Add order types

--- models/filter_payload.py
+++ models/filter_payload.py
@@ -1,6 +1,7 @@
 from pydantic import BaseModel
 from datetime import datetime 
 from typing import Optional, Union, Dict, Any
+from enum import Enum
 
 class InspectionFilters(BaseModel):
     requested_record_id: Optional[int] = None
@@ -11,6 +12,17 @@ class InspectionFilters(BaseModel):
     device_id: Optional[int] = None
     job_id: Optional[int] = None
     is_analysis: Optional[bool] = False
+
+class OrderType(str, Enum):
+    ORDER_TYPE_DATE = "date"
+    ORDER_TYPE_PASS = "pass"
+    ORDER_TYPE_FAIL = "fail"
+    ORDER_TYPE_INVALID = "invalid"
+
+class OrderDirection(str, Enum):
+    ORDER_DIRECTION_ASCENDING = "asc"
+    ORDER_DIRECTION_DESCENDING = "desc"
+
 
 class OrderFilters(BaseModel):
     order_direction: Optional[str] = None
