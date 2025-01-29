3d9457297e2603bed367c687e7e31ec0303c1fab
Alejandro Cambronero <acambronero@innerspec.com>
2025-01-30 00:31:56 +0100

    Change host to allow connections

--- main.py
+++ main.py
@@ -39,5 +39,5 @@ register_static_routes(app)
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run("main:app", host="localhost", port=port)
+    uvicorn.run("main:app", host="0.0.0.0", port=port)
 
