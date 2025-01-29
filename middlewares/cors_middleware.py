58b5a6d7f52067ed800e561b28f35d7a5805e5e6
Alejandro Cambronero <acambronero@innerspec.com>
2025-01-30 00:45:59 +0100

    Allow connections from other addresses

--- middlewares/cors_middleware.py
+++ middlewares/cors_middleware.py
@@ -5,7 +5,7 @@ from config.config import CORS_ORIGINS
 def add_middlewares(app: FastAPI):
     app.add_middleware(
         CORSMiddleware,
-        allow_origins=CORS_ORIGINS,
+        allow_origins=["*"],
         allow_credentials=True,
         allow_methods=["*"],
         allow_headers=["*"],
