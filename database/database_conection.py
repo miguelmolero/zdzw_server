92154d7f3613c80eaf6d2d810d2e7eb015296af8
Alejandro Cambronero <acambronero@innerspec.com>
2025-03-21 20:02:49 +0100

    Change database connection to Sqlite3 no handle concurrent requests

--- database/database_conection.py
+++ database/database_conection.py
@@ -1,16 +1,25 @@
 from typing import Generator
-from sqlalchemy.orm import Session
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, event
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker, Session
+from sqlalchemy.pool import StaticPool
 from config.config import DATABASE_URL
 
 # create the database engine
 engine = create_engine(
     DATABASE_URL, 
     connect_args={"check_same_thread": False},
-    echo=True
+    echo=True,
+    poolclass=StaticPool
 )
+
+@event.listens_for(engine, "connect")
+def set_sqlite_pragma(dbapi_connection, connection_record):
+    cursor = dbapi_connection.cursor()
+    cursor.execute("PRAGMA journal_mode=WAL")
+    cursor.execute("PRAGMA synchronous=NORMAL")
+    cursor.execute("PRAGMA foreign_keys=ON")
+    cursor.close()
 
 # Create session local to perform database operations
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
