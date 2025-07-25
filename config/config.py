8e71e2101500cf7d5f05d651e37dc5da1fe10df1
Alejandro Cambronero <acambronero@innerspec.com>
2025-07-25 20:04:58 +0200

    Update folders path

--- config/config.py
+++ config/config.py
@@ -2,23 +2,24 @@ from pathlib import Path
 import sys
 import secrets
 
-# Determina si la aplicación está empaquetada con PyInstaller
-def get_base_path():
-    if getattr(sys, 'frozen', False):
-        return Path(sys._MEIPASS)
-    return Path(__file__).resolve().parent
+def is_docker():
+    return Path("/.dockerenv").exists()
+
+if is_docker():
+    BASE_PATH = Path("/app")  # Docker environment
+else:
+    BASE_PATH = Path(__file__).resolve().parent / "app"  # Local development environment
 
 # Configuración de CORS
 CORS_ORIGINS = ["http://localhost:5000"]
 
 # Configuración de la ruta de archivos estáticos
-BASE_PATH = get_base_path()
 STATIC_FOLDER_PATH = (BASE_PATH / "../static/dist").resolve()
 
 SECRET_KEY = secrets.token_urlsafe(32)
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 30
-DATABASE_URL = "sqlite:///./zdzw.db"  # database URL
-RECEIVED_RECORDS_PATH = (BASE_PATH / "../../ReceivedRecords").resolve()
-RECEIVED_TEMP_RECORDS_PATH = (BASE_PATH / "../../ReceivedTempRecords").resolve()
-STORED_RECORDS_PATH = (BASE_PATH / "../../RecordsData").resolve()
+DATABASE_URL = f"sqlite:///{BASE_PATH / 'data' / 'zdzw.db'}"
+RECEIVED_RECORDS_PATH = BASE_PATH / "ReceivedRecords"
+RECEIVED_TEMP_RECORDS_PATH = BASE_PATH / "ReceivedTempRecords"
+STORED_RECORDS_PATH = BASE_PATH / "RecordsData"
